779918f12905357892932a8a4eead5d3c0e8144a
Hsin-Hung Wu <henrywu19970511@gmail.com>
2020-12-05 00:58:49 +0800

    modify classifier

--- Parkinglot classifier/pklotClassifier.py
+++ Parkinglot classifier/pklotClassifier.py
@@ -1,32 +1,35 @@
 import csv
+import shutil
+import os
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import load_model
 
 print(tf.__version__)
+print(os.getcwd())
 
 
 model = load_model('my_model.h5')#load the trained model
 
 test_datagen = ImageDataGenerator(rescale=1. / 255)
+
+#create an empty 'predict' folder in the same directory and then put your folder of images into it
 eval_generator = test_datagen.flow_from_directory("predict",target_size=(224, 224),
                                                   batch_size=1,shuffle=False,seed=42,class_mode=None)
 
 pred = model.predict(eval_generator,verbose=1)
 
-with open('prediction.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
+os.mkdir("P")
+os.mkdir("NP")
 
-    for index, probability in enumerate(pred):
+for index, probability in enumerate(pred):
 
-        if (probability[0]>=0.5):
+    if (probability[0]>=0.5):
 
-            writer.writerow([eval_generator.filenames[index], "P"])
+        shutil.move("predict/"+eval_generator.filenames[index], "P")
+    else:
 
-        else:
-
-            writer.writerow([eval_generator.filenames[index], "NP"])
-
+        shutil.move("predict/"+eval_generator.filenames[index], "NP")
         
 
 
